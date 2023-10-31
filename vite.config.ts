9cc0183ba09cb301fa827c9ded04c8925a76bd3c
prithvi-rumsan <maharjanprithvi@gmail.com>
2023-10-31 16:08:00 +0545

    Import configs fixed

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,25 @@
-import legacy from '@vitejs/plugin-legacy'
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import legacy from "@vitejs/plugin-legacy";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    legacy()
-  ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
-  }
-})
+  plugins: [react(), legacy()],
+  build: { chunkSizeWarningLimit: 1600 },
+  resolve: {
+    alias: {
+      "@api": path.resolve(__dirname, "src/api"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@sections": path.resolve(__dirname, "src/sections"),
+      "@services": path.resolve(__dirname, "src/services"),
+      "@store": path.resolve(__dirname, "src/store"),
+      "@theme": path.resolve(__dirname, "src/theme"),
+      "@types": path.resolve(__dirname, "src/types"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+      // Add more aliases as needed
+    },
+  },
+});
