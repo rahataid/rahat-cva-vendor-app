b49c429d9b31d79141f0dbc53d938a165cb0df01
prithvi-rumsan <maharjanprithvi@gmail.com>
2023-11-22 12:25:15 +0545

    React hook form implemented in create and restore wallet form

--- src/sections/auth/registration/index.tsx
+++ src/sections/auth/registration/index.tsx
@@ -15,48 +15,109 @@ import {
 
 import "./register.scss";
 import { useHistory } from "react-router";
+import { useForm, Controller } from "react-hook-form";
+import TextInputField from "@components/input/form-text-input";
 
 const Register = () => {
   const history = useHistory();
   const handleCancel = () => {
     history.goBack();
   };
+
+  const {
+    handleSubmit,
+    control,
+    setValue,
+    getValues,
+    formState: { errors, isDirty, isValid },
+  } = useForm({
+    mode: "all",
+    defaultValues: {
+      name: "",
+      phone: "",
+    },
+  });
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+    alert(JSON.stringify(data, null, 2));
+  };
+
   return (
-    <IonGrid className="register-container">
-      <IonRow className="register-form-container">
-        <IonCol size="11" sizeMd="11" sizeLg="6" sizeXl="4">
-          <IonInput
-            label="Full Name"
-            labelPlacement="floating"
-            fill="solid"
-            placeholder="Full Name"
-          ></IonInput>
-          <br />
-          <IonInput
-            label="Phone"
-            labelPlacement="floating"
-            fill="solid"
-            placeholder="Phone"
-          ></IonInput>
-        </IonCol>
-      </IonRow>
-      <IonRow className="register-button-container">
-        <IonCol size="11" sizeMd="11" sizeLg="6" sizeXl="4">
-          <IonButton expand="block" color="white">
-            Submit
-          </IonButton>
-          <IonRow className="gap-5"></IonRow>
-          <IonButton
-            color="white"
-            fill="outline"
-            expand="block"
-            onClick={handleCancel}
-          >
-            Cancel
-          </IonButton>
-        </IonCol>
-      </IonRow>
-    </IonGrid>
+    <form onSubmit={handleSubmit(onSubmit)} style={{ height: "100%" }}>
+      <IonGrid className="register-container">
+        <IonRow className="register-form-container">
+          <IonCol size="11" sizeMd="11" sizeLg="6" sizeXl="4">
+            <Controller
+              render={({ field }) => (
+                <TextInputField
+                  placeholder="Enter Name"
+                  type="text"
+                  label="Name*"
+                  value={field.value}
+                  errorText={errors.name?.message}
+                  onInput={(e: any) => {
+                    setValue("name", e.detail.value, { shouldValidate: true });
+                  }}
+                  onBlur={field.onBlur}
+                />
+              )}
+              rules={{
+                required: "Please enter your full name",
+              }}
+              control={control}
+              name="name"
+            />
+            <br />
+            <Controller
+              render={({ field }) => (
+                <TextInputField
+                  placeholder="Phone"
+                  type="text"
+                  label="Phone*"
+                  errorText={errors.phone?.message}
+                  value={getValues("phone")}
+                  onInput={(e: { detail: { value: string } }) => {
+                    setValue("phone", e.detail.value, { shouldValidate: true });
+                  }}
+                  onBlur={field.onBlur}
+                />
+              )}
+              rules={{
+                required: { value: true, message: "Please enter Phone number" },
+                minLength: {
+                  value: 6,
+                  message: "Phone Number must be at least 6 digits",
+                },
+              }}
+              control={control}
+              name="phone"
+            />
+          </IonCol>
+        </IonRow>
+        <IonRow className="register-button-container">
+          <IonCol size="11" sizeMd="11" sizeLg="6" sizeXl="4">
+            <IonButton
+              type="submit"
+              expand="block"
+              color="white"
+              disabled={isDirty || !isValid}
+            >
+              Submit
+            </IonButton>
+            <IonRow className="gap-5"></IonRow>
+            <IonButton
+              color="white"
+              fill="outline"
+              expand="block"
+              onClick={handleCancel}
+            >
+              Cancel
+            </IonButton>
+          </IonCol>
+        </IonRow>
+      </IonGrid>
+    </form>
   );
 };
 
--- src/sections/auth/restore-wallet/index.tsx
+++ src/sections/auth/restore-wallet/index.tsx
@@ -10,43 +10,96 @@ import {
 } from "@ionic/react";
 import { useHistory } from "react-router";
 import "./restore.scss";
+import { Controller, useForm } from "react-hook-form";
+import TextInputField from "@components/input/form-text-input";
 
 const RestoreWallet = () => {
   const history = useHistory();
   const handleCancel = () => {
     history.goBack();
   };
+
+  const {
+    handleSubmit,
+    control,
+    setValue,
+    getValues,
+    formState: { errors, isDirty, isValid },
+  } = useForm({
+    mode: "all",
+    defaultValues: {
+      pneumonics: "",
+    },
+  });
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+    alert(JSON.stringify(data, null, 2));
+  };
+
   return (
-    <IonGrid className="restore-container">
-      <IonRow className="restore-form-container">
-        <IonCol size="11" sizeMd="11" sizeLg="6" sizeXl="4">
-          <IonList>
-            <IonItem>
-              <IonTextarea
-                label=""
-                placeholder="Please enter 12 words pneumonics"
-              ></IonTextarea>
-            </IonItem>
-          </IonList>
-        </IonCol>
-      </IonRow>
-      <IonRow className="restore-button-container">
-        <IonCol size="11" sizeMd="11" sizeLg="6" sizeXl="4">
-          <IonButton color="white" expand="block">
-            Submit
-          </IonButton>
-          <IonRow className="gap-5"></IonRow>
-          <IonButton
-            color="white"
-            fill="outline"
-            expand="block"
-            onClick={handleCancel}
-          >
-            Cancel
-          </IonButton>
-        </IonCol>
-      </IonRow>
-    </IonGrid>
+    <form onSubmit={handleSubmit(onSubmit)} style={{ height: "100%" }}>
+      <IonGrid className="restore-container">
+        <IonRow className="restore-form-container">
+          <IonCol size="11" sizeMd="11" sizeLg="6" sizeXl="4">
+            <Controller
+              render={({ field }) => (
+                <TextInputField
+                  placeholder="Please enter 12 words pneumonics"
+                  type="text"
+                  label="Pneumonics*"
+                  errorText={errors.pneumonics?.message}
+                  value={getValues("pneumonics")}
+                  onInput={(e: { detail: { value: string } }) => {
+                    setValue("pneumonics", e.detail.value, {
+                      shouldValidate: true,
+                    });
+                  }}
+                  onBlur={field.onBlur}
+                />
+              )}
+              rules={{
+                required: {
+                  value: true,
+                  message: "Please enter 12 words pneumonics",
+                },
+                pattern: {
+                  value: /^([a-z]+ ){11}[a-z]+$/i,
+                  message: "Invalid pneumonics",
+                },
+              }}
+              control={control}
+              name="pneumonics"
+            />
+            {/* <IonTextarea
+                  label=""
+                  placeholder="Please enter 12 words pneumonics"
+                ></IonTextarea> */}
+          </IonCol>
+        </IonRow>
+        <IonRow className="restore-button-container">
+          <IonCol size="11" sizeMd="11" sizeLg="6" sizeXl="4">
+            <IonButton
+              type="submit"
+              expand="block"
+              color="white"
+              disabled={isDirty || !isValid}
+            >
+              Submit
+            </IonButton>
+            <IonRow className="gap-5"></IonRow>
+            <IonButton
+              color="white"
+              fill="outline"
+              expand="block"
+              onClick={handleCancel}
+            >
+              Cancel
+            </IonButton>
+          </IonCol>
+        </IonRow>
+      </IonGrid>
+    </form>
   );
 };
 
