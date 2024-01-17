334611cbd4149e3103793dbb98095aac71a75944
prithvi-rumsan <maharjanprithvi@gmail.com>
2024-01-17 14:10:50 +0545

    ENV configs changed

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,3 @@
-import { ENV as environment } from "./env/env.dev";
-
-export const ENV = environment.ENV;
-export const DEFAULT_PASSCODE = environment.DEFAULT_PASSCODE;
-export const CONTRACTS = environment.CONTRACTS;
+export const ENV = import.meta.env.VITE_ENV;
+export const DEFAULT_PASSCODE = import.meta.env.VITE_DEFAULT_PASSCODE;
+export const CONTRACTS = JSON.parse(import.meta.env.VITE_CONTRACTS);
