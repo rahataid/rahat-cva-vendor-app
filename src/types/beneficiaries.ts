0b70d1b76d66018013af03fe00818d3c4cc72e7e
prithvi-rumsan <prithvi.maharjan@rumsan.net>
2024-05-27 14:40:22 +0545

    Further cleanups

--- src/types/beneficiaries.ts
+++ src/types/beneficiaries.ts
@@ -6,25 +6,13 @@ export type IBeneficiary = {
   estimatedAge?: ESTIMATED_AGE;
   address?: string;
   status?: STATUS;
-  beneficiaryType?: BENEFICIARY_TYPE;
   walletAddress?: string;
   token?: string;
   otpHash?: string;
-  voucherType?: VOUCHER;
   createdAt: number;
   voucherSymbol?: string;
   transactionHash?: string;
 };
-
-export enum BENEFICIARY_TYPE {
-  REFERRED = "REFERRED",
-  ENROLLED = "ENROLLED",
-}
-
-export enum VOUCHER {
-  FREE_VOUCHER = "FREE_VOUCHER",
-  DISCOUNT_VOUCHER = "DISCOUNT_VOUCHER",
-}
 
 export enum STATUS {
   SUCCESS = "SUCCESS",
@@ -66,55 +54,6 @@ export enum DATE_SOURCE {
   BLOCKCHAIN = "BLOCKCHAIN",
 }
 
-export type BENEFICIARY_VOUCHER_DETAILS = {
-  FreeVoucherAddress?: string;
-  FreeVoucherClaimStatus?: boolean;
-  ReferredVoucherAddress?: string;
-  ReferredVoucherClaimStatus?: boolean;
-  FreeVoucherRevertStatus?: boolean;
-  DiscountVoucherRevertStatus?: boolean;
-  beneficiaryAddress: string;
-  id: string;
-  __typename: string;
-};
-
-export type REFER_BENEFICIARY_DETAILS = {
-  name: string;
-  phone: string;
-  gender: GENDER;
-  estimatedAge: ESTIMATED_AGE;
-  address: string;
-  walletAddress?: string;
-  code?: string;
-  fullPhone?: string;
-};
-
-export type CreateBeneficiaryDto = {
-  walletAddress?: string;
-  referrerBeneficiary?: string; //UUID
-  referrerVendor?: string; //UUID
-  extras?: any;
-  type?: BENEFICIARY_TYPE;
-  age?: number | string;
-  piiData: {
-    name: string;
-    phone: string;
-  };
-};
-
-export type REFERRED_BENEFICIARY_DETAILS = {
-  id?: number;
-  uuid?: string;
-  walletAddress?: string;
-  extras?: any;
-  eyeCheckUp?: boolean;
-  glassRequired?: boolean;
-  beneficiariesReferred?: number;
-  referrerBeneficiary?: string;
-  referrerVendor?: string;
-  type?: BENEFICIARY_TYPE;
-};
-
 export interface BENEFICIARY_DETAILS {
   id?: number;
   uuid?: string;
@@ -144,64 +83,3 @@ export interface BENEFICIARY_DETAILS {
     extras?: any;
   };
 }
-
-export type REFER_RESULT_BENEFICIARY_DETAILS = {
-  address: string;
-  estimatedAge: string;
-  gender: GENDER;
-  name: string;
-  phone: string;
-  transactionHash: string;
-  walletAddress: string;
-  createdAt?: string;
-};
-
-export type PROJECT_DETAILS = {
-  Project: {
-    contractAddress?: string;
-    createdAt?: string;
-    deletedAt?: string;
-    description?: string;
-    extras?: any;
-    id?: number;
-    name?: string;
-    status?: ANY;
-    type?: string;
-    updatedAt?: string;
-    uuid?: string;
-  };
-  beneficiaryId?: string;
-  createdAt?: string;
-  deletedAt?: string;
-  id?: number;
-  projectId?: string;
-  updatedAt?: string;
-  uuid?: string;
-};
-
-export interface BENEFICIARY_REFERRAL_DETAILS extends BENEFICIARY_DETAILS {
-  BeneficiaryProject?: PROJECT_DETAILS[] | [];
-  beneficiariesReferred?: number;
-}
-
-export type BENEFICIARY_VOUCHER_STATUS_GRAPH = {
-  FreeVoucherAddress: string | null;
-  ReferredVoucherAddress: string | null;
-  FreeVoucherClaimStatus: boolean | null;
-  ReferredVoucherClaimStatus: boolean | null;
-  FreeVoucherRevertStatus?: boolean | null;
-  ReferredVoucherRevertStatus?: boolean | null;
-  beneficiaryAddress?: string;
-  error?: any;
-  id?: string;
-  __typename?: string;
-};
-
-export type BENEFICIARY_VOUCHER_STATUS_CONTRACT = {
-  0: string | null;
-  1: string | null;
-  2: boolean | null;
-  3: boolean | null;
-  4: boolean | null;
-  5: boolean | null;
-};
--- src/types/chargeBeneficiary.ts
+++
@@ -1,4 +0,0 @@
-export type SelectInputOptions = {
-  label: string;
-  value: string;
-}[];
